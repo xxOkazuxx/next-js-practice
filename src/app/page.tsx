4f8cb1c87ece219abb598aa3efd46b384001e5b8
Okazu <kazu910739@gmail.com>
2024-11-11 23:17:13 +0900

    aboutボタンの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Suspense } from "react";
 import { ServerComponent } from "./_components/ServerComponent";
 import { SkeletonComponent } from "./_components/SkeletonComponent";
@@ -7,10 +8,11 @@ export default function Home() {
   // const randomId: number = Math.floor(Math.random() * 100);
   console.log("React server component")
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center flex-col">
       <Suspense fallback={<SkeletonComponent width="600px" height="500px" />}>
         <ServerComponent />
       </Suspense>
+      <Link href="/about" className="rounded-md bg-orange-500 text-white px-5 py-3 mt-4 hover:bg-orange-600">About</Link>
     </div>
   );
 }
